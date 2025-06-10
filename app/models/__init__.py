9bb63e956879e0000dc7ad2325c9a6b3ccc6453f
SergeyGrishencko <grisenchen@gmail.com>
2025-06-10 22:39:29 +0700

    add models

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+__all__ = (
+    "Base",
+    "Goal",
+    "Project",
+    "Task",
+    "User",
+    "UserProjectAssociation",
+)
+
+from .base import Base
+from .goal import Goal
+from .project import Project
+from .task import Task
+from .user import User
+from .users_projects_association import UserProjectAssociation
