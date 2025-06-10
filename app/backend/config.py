618f68d933973ef55f0a90e26c5b0d89c8950b66
SergeyGrishencko <grisenchen@gmail.com>
2025-06-10 22:39:54 +0700

    add authjwt model

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,5 +1,15 @@
 from pydantic_settings import BaseSettings
-from pydantic import model_validator
+from pydantic import model_validator, BaseModel
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
 
 class Settings(BaseSettings):
     DB_HOST: str
@@ -7,6 +17,8 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+
+    auth_jwt: AuthJWT = AuthJWT()
 
     @model_validator(mode="before")
     def get_database_url(cls, v):
